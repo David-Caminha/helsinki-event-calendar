572bfd36af2d62c8bb8ed131cb698ac568f2dec8
David-Caminha <david.caminha94@gmail.com>
2016-12-28 07:19:26 +0000

    more database queries and event creation

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,70 +5,96 @@
 angular.module('myApp.controllers', []).
 controller('AppCtrl', function($scope, $http) {
 
-    $http({
-        method: 'GET',
-        url: '/api/name'
-    }).
-    success(function(data, status, headers, config) {
-        $scope.name = data.name;
-    }).
-    error(function(data, status, headers, config) {
-        $scope.name = 'Error!';
-    });
+  $http({
+    method: 'GET',
+    url: '/api/name'
+  }).
+  success(function(data, status, headers, config) {
+    $scope.name = data.name;
+  }).
+  error(function(data, status, headers, config) {
+    $scope.name = 'Error!';
+  });
 
 }).
 controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
-    //get all events
-    $http({
-        method: 'GET',
-        url: '/api/events'
-    }).
-    success(function(data, status, headers, config) {
-        $scope.events = data;
-        console.log(data);
-    }).
-    error((error) => {
-        $scope.events = [];
-        console.log('Error: ' + error);
-    });
+  //get all events
+  $http({
+    method: 'GET',
+    url: '/api/events'
+  }).
+  success((data, status, headers, config) => {
+    $scope.events = data;
+  }).
+  error((error) => {
+    $scope.events = [];
+    console.log('Error: ' + error);
+  });
 
-    $scope.months = [];
-    $scope.months.push("February");
-    $scope.months.push("March");
-    $scope.months.push("April");
-    $scope.months.push("May");
-    $scope.months.push("June");
-    $scope.months.push("July");
-    $scope.months.push("August");
-    $scope.months.push("September");
-    $scope.months.push("October");
-    $scope.months.push("November");
-    $scope.months.push("December");
+  $scope.months = [];
+  $scope.months.push("February");
+  $scope.months.push("March");
+  $scope.months.push("April");
+  $scope.months.push("May");
+  $scope.months.push("June");
+  $scope.months.push("July");
+  $scope.months.push("August");
+  $scope.months.push("September");
+  $scope.months.push("October");
+  $scope.months.push("November");
+  $scope.months.push("December");
 
 
-    $scope.myInterval = 3000;
-    $scope.slides = [{
-        image: 'http://lorempixel.com/400/200/'
-    }, {
-        image: 'http://lorempixel.com/400/200/food'
-    }, {
-        image: 'http://lorempixel.com/400/200/sports'
-    }, {
-        image: 'http://lorempixel.com/400/200/people'
-    }];
+  $scope.myInterval = 3000;
+  $scope.slides = [{
+    image: 'http://lorempixel.com/400/200/'
+  }, {
+    image: 'http://lorempixel.com/400/200/food'
+  }, {
+    image: 'http://lorempixel.com/400/200/sports'
+  }, {
+    image: 'http://lorempixel.com/400/200/people'
+  }];
 }]).
-controller('MyCtrl1', function($scope) {
-    // write Ctrl here
+controller('EventCreaterCtrl', function($scope, $http) {
+
+  $scope.formData = {};
+  $scope.formData.price = 0;
+
+  // Create a new event
+  $scope.createEvent = () => {
+    $http.post('/api/events', $scope.formData)
+    .success((data) => {
+      $scope.formData = {};
+    })
+    .error((error) => {
+      console.log('Error: ' + error);
+    });
+  };
 
 }).
-controller('MyCtrl2', function($scope) {
-    // write Ctrl here
+controller('EventPageCtrl', function($scope, $http, $routeParams) {
+
+  console.log("route params: " + $routeParams.event_id);
+  //get one event
+  $http({
+    method: 'GET',
+    url: '/api/events/' + $routeParams.event_id
+  }).
+  success((data, status, headers, config) => {
+    $scope.event = data[0];
+    console.log($scope.event);
+  }).
+  error((error) => {
+    $scope.event = {};
+    console.log('Error: ' + error);
+  });
 
 }).
 controller('Controller', ['$scope', function($scope) {
-    $scope.customer = {
-        name: 'Naomi',
-        address: '1600 Amphitheatre'
-    };
+  $scope.customer = {
+    name: 'Naomi',
+    address: '1600 Amphitheatre'
+  };
 }]);
